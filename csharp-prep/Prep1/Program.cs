cb2c588b4b43092038a0ac0a35425f72572ff1bf
therealdebrown <debrownf@gmail.com>
2024-11-11 18:42:50 +0100

    core requirement

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,13 +1,49 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.Write("What is your first name? ");
-        string first = Console.ReadLine();
-        Console.Write("What is your last name? ");
-        string last = Console.ReadLine();
-        Console.WriteLine($"Your name is {first} {last}");
+    { 
+        Console.WriteLine("What is your grade? ");
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out int grade))
+        { 
+            if (grade >= 90)
+            {
+                Console.WriteLine("Your grade is greater than or equal to 90?");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("Your grade is greater than or equal to 80");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Your grade is greater than or equal to 70");
+            }
+            else if ( grade >= 60) 
+            {
+                Console.WriteLine("Your grade is greater than or equal to 60");
+            }
+            else if (grade < 60)
+            {
+                Console.WriteLine("Your grade is less than 60");
+            }
+
+            if (grade >= 60)
+            {
+                Console.WriteLine("Congratulations for passing");
+            }
+            else
+            {
+
+                Console.WriteLine("Sorry you will do better next time");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Enter a valid number.");
+        }
     }
+
 }
