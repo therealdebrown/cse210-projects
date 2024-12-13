491261d90b9ffa588193ad617c77e6cc7951aa68
therealdebrown <debrownf@gmail.com>
2024-12-13 17:21:28 +0100

    Learning06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square square = new Square("Red", 4);
+
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+
+        Circle circle = new Circle("Green", 2.5);
+
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle
+        };
+        Console.WriteLine("\nShapes in the List:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
